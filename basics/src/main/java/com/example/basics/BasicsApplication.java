a33afc7b196c06c99a3d5e1194c21b7151e7a4c6
Josh Long <josh@joshlong.com>
2021-03-27 16:42:08 -0700

    up

--- basics/src/main/java/com/example/basics/BasicsApplication.java
+++ basics/src/main/java/com/example/basics/BasicsApplication.java
@@ -4,16 +4,17 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.geo.*;
+import org.springframework.data.geo.Circle;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.Point;
 import org.springframework.data.redis.connection.RedisGeoCommands;
-import org.springframework.data.redis.core.GeoOperations;
 import org.springframework.data.redis.core.ReactiveGeoOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import reactor.core.publisher.Flux;
 
 import java.util.Map;
 
-// todo introduce geography!!
 @SpringBootApplication
 public class BasicsApplication {
 
